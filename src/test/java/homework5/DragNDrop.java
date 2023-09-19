5c7f0ab7580cb91b29a0ebd48a08774edce3b48d
seleverstov <Maksim.Seleverstov@lukoil.com>
2023-09-19 11:26:11 +0300

    add homework5 package

---
+++ src/test/java/homework5/DragNDrop.java
@@ -0,0 +1,31 @@
+package homework5;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragNDrop {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = false;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser="edge";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void dragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").$("header").shouldHave(text("A"));
+        $("#column-b").$("header").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(to("#column-b"));
+        $("#column-a").$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A"));
+    }
+}
