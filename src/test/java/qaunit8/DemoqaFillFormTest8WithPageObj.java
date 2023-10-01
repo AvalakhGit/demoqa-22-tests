12ffaeb62c6a2e848e7873e5c3b4f36f5c3bba0f
seleverstov <Maksim.Seleverstov@lukoil.com>
2023-10-01 15:11:59 +0300

    reformat

--- src/test/java/qaunit8/DemoqaFillFormTest8WithPageObj.java
+++ src/test/java/qaunit8/DemoqaFillFormTest8WithPageObj.java
@@ -1,13 +1,8 @@
 package qaunit8;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import pages.RegistrationPage;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-
-public class DemoqaFillFormTest8WithPageObj extends TestBase{
+public class DemoqaFillFormTest8WithPageObj extends TestBase {
 
     String userFirstName = "UserFirstName";
     String userLastName = "UserLastName";
@@ -16,11 +11,12 @@ public class DemoqaFillFormTest8WithPageObj extends TestBase{
     String userAddress = "User Address. City One.";
     String userMobile = "9991234560";
     String userMobileBad = "99912560";
-    String[] userDateBirth = {"11","November","1979"};
-    String[] userSubjects={"Maths","Physics"};
-    String[] userHobbies= {"Sports","Reading"};
+    String[] userDateBirth = {"11", "November", "1979"};
+    String[] userSubjects = {"Maths", "Physics"};
+    String[] userHobbies = {"Sports", "Reading"};
     String userState = "NCR";
     String userCity = "Delhi";
+
     @Test
     void fillFields() {
         registrationPage
@@ -30,9 +26,9 @@ public class DemoqaFillFormTest8WithPageObj extends TestBase{
                 .setEmail(userEmail)
                 .setGender(userGender)
                 .setUserNumber(userMobile)
-                .setDateOfBirth(userDateBirth[0],userDateBirth[1],userDateBirth[2])
-                .setSubject(userSubjects[0],userSubjects[1])
-                .setHobbies(userHobbies[0],userHobbies[1])
+                .setDateOfBirth(userDateBirth[0], userDateBirth[1], userDateBirth[2])
+                .setSubject(userSubjects[0], userSubjects[1])
+                .setHobbies(userHobbies[0], userHobbies[1])
                 .uploadPicture("img/AtomicHeart_sample.jpg")
                 .setAddress(userAddress)
                 .setState(userState)
@@ -42,19 +38,20 @@ public class DemoqaFillFormTest8WithPageObj extends TestBase{
         registrationPage
                 .checkResultModalVisible()
                 .checkResultModalTitleHaveMessage("Thanks for submitting the form")
-                .checkResult ("Student Name",userFirstName+" "+userLastName)
-                .checkResult ("Student Email",userEmail)
-                .checkResult ("Gender",userGender)
-                .checkResult ("Mobile",userMobile)
-                .checkResult ("Date of Birth",userDateBirth[0]+" "+userDateBirth[1]+","+userDateBirth[2])
-                .checkResult ("Subjects",userSubjects[0]+", "+userSubjects[1])
-                .checkResult ("Hobbies",userHobbies[0]+", "+userHobbies[1])
-                .checkResult ("Picture","AtomicHeart_sample.jpg")
-                .checkResult ("Address",userAddress)
-                .checkResult ("State and City",userState+" "+userCity)
+                .checkResult("Student Name", userFirstName + " " + userLastName)
+                .checkResult("Student Email", userEmail)
+                .checkResult("Gender", userGender)
+                .checkResult("Mobile", userMobile)
+                .checkResult("Date of Birth", userDateBirth[0] + " " + userDateBirth[1] + "," + userDateBirth[2])
+                .checkResult("Subjects", userSubjects[0] + ", " + userSubjects[1])
+                .checkResult("Hobbies", userHobbies[0] + ", " + userHobbies[1])
+                .checkResult("Picture", "AtomicHeart_sample.jpg")
+                .checkResult("Address", userAddress)
+                .checkResult("State and City", userState + " " + userCity)
                 .closeResultModal();
 
     }
+
     @Test
     public void successRegisterRequiredFieldsTest() {
         registrationPage.openPage()
@@ -97,6 +94,7 @@ public class DemoqaFillFormTest8WithPageObj extends TestBase{
 
         registrationPage.checkResultModalHidden();
     }
+
     @Test
     public void negativeRegisterEmptyFieldsTest() {
         registrationPage.openPage()
