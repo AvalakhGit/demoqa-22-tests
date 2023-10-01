8f200fc20c9b38589c56182afe85f05615683cf5
seleverstov <Maksim.Seleverstov@lukoil.com>
2023-10-01 19:43:28 +0300

    home work commit final

--- src/test/java/utils/RandomUtils.java
+++ src/test/java/utils/RandomUtils.java
@@ -1,9 +1,7 @@
 package utils;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
@@ -16,25 +14,10 @@ public class RandomUtils {
         int index = getRandomInt(0, array.length - 1);
         return array[index];
     }
-    public static String getRandomItemFromArray(String[] array,int count){
-        int index = getRandomInt(0, array.length - 1);
-        return array[index];
-    }
+
     public static String getItemFromDate(String pattern, Date date) {
         SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.ENGLISH);
         return formatter.format(date);
     }
-
-    public static ArrayList<String> getItemsRandom(String[] array, int count){
-        ArrayList<String> list = new ArrayList<String>(count);
-        int index=0;
-        for (int j=0;j<=count;j++){
-            index = getRandomInt(0, array.length - 1);
-
-            list.add(array[index]);
-        }
-        return list;
-    }
-
 
 }
