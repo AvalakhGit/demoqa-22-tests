521809eb5db5c6dc13530cfcfeb8a22fa5579357
seleverstov <Maksim.Seleverstov@lukoil.com>
2023-10-13 22:26:01 +0300

    add video

--- src/test/java/RegistrationRemoteTests.java
+++ src/test/java/RegistrationRemoteTests.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
@@ -27,6 +30,14 @@ public class RegistrationRemoteTests {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities=capabilities;
     }
 
     @AfterEach
